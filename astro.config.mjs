64981874fa945a2a1e0879502c1d3f97c6fba0ba
Sami Fouad <6378290+samifouad@users.noreply.github.com>
2024-04-18 00:23:21 -0600

    switching to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercelServerless(),
   integrations: [tailwind(), svelte()]
 });
