8798b6c7992dd987eb6b4712137f45aa849a7396
Sami Fouad <6378290+samifouad@users.noreply.github.com>
2024-04-18 00:38:29 -0600

    switching to vercel

--- api/supa.js
+++ api/supa.js
@@ -1,4 +1,9 @@
-import { supabase } from '../src/db/supabase'
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function GET() {
 
