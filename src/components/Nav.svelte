745f2fc4c6757c5ca71f57c829b7f19297d7314a
Sami Fouad <6378290+samifouad@users.noreply.github.com>
2024-04-20 17:17:49 -0600

    adding gh sign in

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -9,7 +9,7 @@ let user = { loggedIn: false };
 
 onMount(async () => {
 	try {
-		const { data, error } = await supabase.auth.getUser()
+		const { data, error } = await supabase.auth.getSession()
 
 		console.log(data.user)
 
